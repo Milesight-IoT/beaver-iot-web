793c0e5ed18af29d249d36e6a4bd2fa6a783dd4e
kangyiban <kangyb@milesight.com>
2025-03-27 10:06:33 +0800

    feat: Integration support embedded ns

---
+++ apps/web/src/pages/integration/views/integration-detail/components/ns-content/config/component/index.tsx
@@ -0,0 +1,3 @@
+export { CopyTextField, CopyPwdTextField } from './copy-text';
+export { GatewayMqttInfo, type FormDataProps } from './gateway-mqtt';
+export { default as GatewayStatus } from './status';
