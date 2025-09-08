0b3e9eac2148e666704158eb20f7b4f9bef70802
jimco <xiejc@milesight.com>
2025-09-08 17:56:16 +0800

    feat: Add mobile device page

---
+++ apps/web/src/components/mobile-qrcode-scanner/index.tsx
@@ -0,0 +1,9 @@
+const MobileQRCodeScanner = () => {
+    return (
+        <div className="ms-mobile-qrcode-scanner">
+            <div>QR Code Scanner</div>
+        </div>
+    );
+};
+
+export default MobileQRCodeScanner;
