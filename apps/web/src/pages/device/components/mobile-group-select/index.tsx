604594ec42ecae3eb8c52619349ca0a1395e7e3e
jimco <xiejc@milesight.com>
2025-10-20 10:56:21 +0800

    fix: Lock page when loading to avoid the quick selected event mess

--- apps/web/src/pages/device/components/mobile-group-select/index.tsx
+++ apps/web/src/pages/device/components/mobile-group-select/index.tsx
@@ -2,7 +2,7 @@ import React, { memo, useState, useMemo, useLayoutEffect } from 'react';
 import cls from 'classnames';
 import { useRequest, useControllableValue } from 'ahooks';
 import { useI18n } from '@milesight/shared/src/hooks';
-import { Modal, KeyboardArrowDownIcon } from '@milesight/shared/src/components';
+import { Modal, LoadingWrapper, KeyboardArrowDownIcon } from '@milesight/shared/src/components';
 import { Tooltip } from '@/components';
 import {
     deviceAPI,
@@ -33,8 +33,10 @@ const MobileGroupSelect: React.FC<Props> = memo(props => {
 
     // ---------- Render Group List ----------
     const { deviceGroups, updateDeviceGroups, updateActiveGroup } = useDeviceStore();
+    const [loading, setLoading] = useState<boolean>();
     const { run: getDeviceGroups } = useRequest(
         async () => {
+            setLoading(true);
             const [error, resp] = await awaitWrap(
                 deviceAPI.getDeviceGroupList({
                     page_number: 1,
@@ -42,6 +44,7 @@ const MobileGroupSelect: React.FC<Props> = memo(props => {
                 }),
             );
 
+            setTimeout(() => setLoading(false), 0);
             if (error || !isRequestSuccess(resp)) return;
             const groups = getResponseData(resp)?.content || [];
 
@@ -72,12 +75,14 @@ const MobileGroupSelect: React.FC<Props> = memo(props => {
     // Refresh device groups when open modal
     useLayoutEffect(() => {
         if (!open) return;
+        setLoading(true);
         getDeviceGroups();
     }, [open, getDeviceGroups]);
 
     // ---------- Render Selected Group ----------
     const [selectedGroup, setSelectedGroup] = useState<GroupItem>();
 
+    // Initialize
     useLayoutEffect(() => {
         if (!value) {
             setSelectedGroup(options[0]);
@@ -113,17 +118,20 @@ const MobileGroupSelect: React.FC<Props> = memo(props => {
                     );
                 }}
             >
-                <div className="group-list">
+                <LoadingWrapper loading={loading} wrapperClassName={cls('group-list', { loading })}>
                     {options.map(item => (
                         <div
                             key={item.id}
                             className={cls('group-item', { active: selectedGroup?.id === item.id })}
-                            onClick={() => setSelectedGroup(item)}
+                            onClick={() => {
+                                if (loading) return;
+                                setSelectedGroup(item);
+                            }}
                         >
                             <Tooltip autoEllipsis title={item.name} />
                         </div>
                     ))}
-                </div>
+                </LoadingWrapper>
             </Modal>
         </div>
     );
