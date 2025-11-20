b9a46f6ffeb4e20703a7c7cc849463c48c84ebe0
jimco <xiejc@milesight.com>
2025-11-20 11:42:10 +0800

    fix: Remove redundant code

--- apps/web/src/hooks/useMqtt.ts
+++ apps/web/src/hooks/useMqtt.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { useRequest, useDebounceEffect } from 'ahooks';
+import { useRequest } from 'ahooks';
 import { useUserStore } from '@/stores';
 import { MqttService, MQTT_STATUS, MQTT_EVENT_TYPE, BATCH_PUSH_TIME } from '@/services/mqtt';
 import { credentialsApi, awaitWrap, getResponseData, isRequestSuccess } from '@/services/http';
@@ -58,26 +58,10 @@ const useMqtt = () => {
                 const debug = window.sessionStorage.getItem('vconsole') === 'true';
                 const mqttClient = new MqttService({ debug, ...data });
 
-                console.warn('【MQTT】', 'init 000');
                 setClient(mqttClient);
             },
         },
     );
-
-    // useDebounceEffect(
-    //     () => {
-    //         console.warn('【MQTT】', 'init 111');
-    //         if (client || !data || Object.values(data).some(item => !item)) return;
-
-    //         console.warn('【MQTT】', 'init 222');
-    //         const debug = window.sessionStorage.getItem('vconsole') === 'true';
-    //         const mqttClient = new MqttService({ debug, ...data });
-    //         console.warn('【MQTT】', 'init 333');
-    //         setClient(mqttClient);
-    //     },
-    //     [data, client, setClient],
-    //     { wait: 300 },
-    // );
 
     return {
         status: client?.status || MQTT_STATUS.DISCONNECTED,
