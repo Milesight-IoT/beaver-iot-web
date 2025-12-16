89dc042091944c0666e14be6dee2573a17c917c7
wuzb <wuzb@yeastar.com>
2025-12-16 21:14:02 +0800

    feat: Drawing board occupancy marker plugin batch bind entities ui design update

--- apps/web/src/components/drawing-board/plugin/components/toilet-bind-entities/BindEntitiesModal.tsx
+++ apps/web/src/components/drawing-board/plugin/components/toilet-bind-entities/BindEntitiesModal.tsx
@@ -3,7 +3,14 @@ import { useForm, Controller, type SubmitHandler } from 'react-hook-form';
 import { useMemoizedFn } from 'ahooks';
 import classNames from 'classnames';
 
-import { Modal, type ModalProps, LoadingWrapper } from '@milesight/shared/src/components';
+import {
+    Modal,
+    type ModalProps,
+    CheckCircleIcon,
+    CancelIcon,
+    FileDownloadOutlinedIcon,
+    LoadingWrapper,
+} from '@milesight/shared/src/components';
 import { useI18n } from '@milesight/shared/src/hooks';
 
 import { type FileValueType } from '@/components';
@@ -25,9 +32,22 @@ const BatchAddModal: React.FC<ModalProps> = props => {
     const { getIntlText } = useI18n();
     const { control, formState, handleSubmit, reset } = useForm<BatchAddProps>();
     const { formItems } = useFormItems();
-    const { handleUploadFile } = useBatchBind();
+    const {
+        handleUploadFile,
+        parseResult,
+        currentStatus,
+        handleDownloadErrorFile,
+        setDownloading,
+        downloading,
+    } = useBatchBind();
 
     const onSubmit: SubmitHandler<BatchAddProps> = async params => {
+        if (currentStatus === 'completed') {
+            reset();
+            onCancel?.();
+            return;
+        }
+
         /**
          * Upload file
          */
@@ -45,21 +65,102 @@ const BatchAddModal: React.FC<ModalProps> = props => {
         onCancel?.();
     });
 
+    const renderBody = () => {
+        if (currentStatus === 'before') {
+            return formItems.map(item => (
+                <Controller<BatchAddProps> {...item} key={item.name} control={control} />
+            ));
+        }
+
+        const { success_data: successData, failed_data: failedData } = parseResult?.data || {};
+        return (
+            <div className="batch-bind-result">
+                <div className="success-item">
+                    <CheckCircleIcon color="success" />
+                    <div className="success-count">
+                        {getIntlText('dashboard.tip.batch_bind_entities_success', {
+                            1: successData?.count || 0,
+                        })}
+                    </div>
+                </div>
+                <div className="failed-item">
+                    <CancelIcon color="error" />
+                    <div className="failed-count">
+                        <div>
+                            {getIntlText('dashboard.tip.batch_bind_entities_failed', {
+                                1: failedData?.count || 0,
+                            })}
+                        </div>
+                        <LoadingWrapper size={20} loading={downloading}>
+                            <div
+                                className="download-wrapper"
+                                onClick={() => {
+                                    setDownloading(true);
+                                    handleDownloadErrorFile();
+                                }}
+                            >
+                                <FileDownloadOutlinedIcon color="inherit" />
+                                <div key="download-text" className="download-text">
+                                    {getIntlText('common.button.download')}
+                                </div>
+                            </div>
+                        </LoadingWrapper>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <Modal
-            size="lg"
+            size={currentStatus === 'completed' ? 'md' : 'lg'}
             visible={visible}
             title={getIntlText('dashboard.label.batch_bind_entities')}
             className={classNames({ loading: formState.isSubmitting })}
             onOk={handleSubmit(onSubmit)}
             onCancel={handleCancel}
+            cancelButtonProps={{
+                style: {
+                    display: currentStatus === 'completed' ? 'none' : undefined,
+                },
+            }}
+            sx={{
+                '& .batch-bind-result': {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    fontSize: '14px',
+                    lineHeight: '22px',
+                    color: 'var(--text-color-primary)',
+                    '.success-item': {
+                        display: 'flex',
+                        alignItems: 'center',
+                        marginBottom: 1,
+                        '.success-count': {
+                            marginLeft: 1,
+                        },
+                    },
+                    '.failed-item': {
+                        display: 'flex',
+                        alignItems: 'center',
+                        '.failed-count': {
+                            display: 'flex',
+                            marginLeft: 1,
+                            '.download-wrapper': {
+                                display: 'flex',
+                                marginLeft: 1,
+                                cursor: 'pointer',
+                                color: 'var(--primary-color-base)',
+                                '.download-text': {
+                                    marginLeft: 0.5,
+                                },
+                            },
+                        },
+                    },
+                },
+            }}
             {...restProps}
         >
-            <LoadingWrapper loading={false}>
-                {formItems.map(item => (
-                    <Controller<BatchAddProps> {...item} key={item.name} control={control} />
-                ))}
-            </LoadingWrapper>
+            {renderBody()}
         </Modal>
     );
 };
--- apps/web/src/components/drawing-board/plugin/components/toilet-bind-entities/useBatchBind.tsx
+++ apps/web/src/components/drawing-board/plugin/components/toilet-bind-entities/useBatchBind.tsx
@@ -1,16 +1,12 @@
-import { useMemoizedFn } from 'ahooks';
+import { useState } from 'react';
+import { useMemoizedFn, useDebounceFn } from 'ahooks';
 import dayjs from 'dayjs';
 
-import {
-    CheckCircleIcon,
-    CancelIcon,
-    FileDownloadOutlinedIcon,
-    toast,
-} from '@milesight/shared/src/components';
+import { toast } from '@milesight/shared/src/components';
 import { useI18n, useStoreShallow } from '@milesight/shared/src/hooks';
 import { linkDownload } from '@milesight/shared/src/utils/tools';
 
-import { type FileValueType, useConfirm } from '@/components';
+import { type FileValueType } from '@/components';
 import useControlPanelStore from '@/components/drawing-board/plugin/store';
 import {
     DashboardAPISchema,
@@ -21,13 +17,22 @@ import {
 } from '@/services/http';
 import { type ToiletBuildingProps } from '../../types';
 
+export type CurrentStatus = 'before' | 'completed';
+
 type ParseRes = DashboardAPISchema['parseToiletBindTemplate']['response'];
 
 export function useBatchBind() {
     const { getIntlText } = useI18n();
-    const confirm = useConfirm();
     const { formData } = useControlPanelStore(useStoreShallow(['formData']));
     const { buildingInfo } = formData || {};
+
+    const [currentStatus, setCurrentStatus] = useState<CurrentStatus>('before');
+    const [parseResult, setParseResult] = useState<{
+        key: ApiKey;
+        file: File;
+        data: ParseRes;
+    }>();
+    const [downloading, setDownloading] = useState(false);
 
     const handleSuccessData = useMemoizedFn((successData?: ParseRes['success_data']) => {
         if (!successData?.count) {
@@ -37,139 +42,52 @@ export function useBatchBind() {
         console.log('handleSuccessData ? ', successData);
     });
 
-    const handleDownloadErrorFile = useMemoizedFn(
-        async (commonParams: { key: ApiKey; file: File }, failedData?: ParseRes['failed_data']) => {
-            if (!failedData?.count || !commonParams.key || !commonParams.file) {
-                return;
-            }
+    const { run: handleDownloadErrorFile } = useDebounceFn(
+        async () => {
+            try {
+                const { key, file } = parseResult || {};
+                const failedData = parseResult?.data?.failed_data;
+                if (!failedData?.count || !key || !file) {
+                    return;
+                }
 
-            const [error, resp] = await awaitWrap(
-                dashboardAPI.generateToiletBindErrorFile(
-                    {
-                        building_key: commonParams.key,
-                        file: commonParams.file,
-                        errors: JSON.stringify({
-                            errors: (failedData?.items || []).map(item => {
-                                return {
-                                    id: item.id,
-                                    msgs: (item?.error_data || []).map(e =>
-                                        getIntlText(`error.http.${e.error_code}`),
-                                    ),
-                                };
+                const [error, resp] = await awaitWrap(
+                    dashboardAPI.generateToiletBindErrorFile(
+                        {
+                            building_key: key,
+                            file,
+                            errors: JSON.stringify({
+                                errors: (failedData?.items || []).map(item => {
+                                    return {
+                                        id: item.id,
+                                        msgs: (item?.error_data || []).map(e =>
+                                            getIntlText(`error.http.${e.error_code}`),
+                                        ),
+                                    };
+                                }),
                             }),
-                        }),
-                    },
-                    {
-                        responseType: 'blob',
-                    },
-                ),
-            );
-            if (error || !isRequestSuccess(resp)) {
-                return;
-            }
-
-            const data = getResponseData(resp);
-            if (!data) {
-                return;
-            }
-
-            linkDownload(data, `${dayjs().format('YYYY_MM_DD_HH_mm_ss')}_error_messages.xlsx`);
-            toast.success(getIntlText('common.message.operation_success'));
-        },
-    );
-
-    const handleFailedData = useMemoizedFn(
-        (commonParams: { key: ApiKey; file: File }, data?: ParseRes) => {
-            if (!data) {
-                return;
-            }
-
-            const { success_data: successData, failed_data: failedData } = data || {};
-
-            confirm({
-                title: getIntlText('dashboard.title.batch_bind_result'),
-                dialogContentTextProps: {
-                    component: 'div',
-                    sx: {
-                        '& .batch-bind-result': {
-                            display: 'flex',
-                            flexDirection: 'column',
-                            fontSize: '14px',
-                            lineHeight: '22px',
-                            color: 'var(--text-color-primary)',
-                            '.success-item': {
-                                display: 'flex',
-                                alignItems: 'center',
-                                marginBottom: 1,
-                                '.success-count': {
-                                    marginLeft: 1,
-                                },
-                            },
-                            '.failed-item': {
-                                display: 'flex',
-                                alignItems: 'center',
-                                '.failed-count': {
-                                    display: 'flex',
-                                    marginLeft: 1,
-                                    '.download-wrapper': {
-                                        display: 'flex',
-                                        marginLeft: 1,
-                                        cursor: 'pointer',
-                                        color: 'var(--primary-color-base)',
-                                        '.download-text': {
-                                            marginLeft: 0.5,
-                                        },
-                                    },
-                                },
-                            },
                         },
-                    },
-                },
-                description: (
-                    <div className="batch-bind-result">
-                        <div key="success-item" className="success-item">
-                            <CheckCircleIcon key="CheckCircleIcon" color="success" />
-                            <div key="success-count" className="success-count">
-                                {getIntlText('dashboard.tip.batch_bind_entities_success', {
-                                    1: successData?.count || 0,
-                                })}
-                            </div>
-                        </div>
-                        <div key="failed-item" className="failed-item">
-                            <CancelIcon key="CancelIcon" color="error" />
-                            <div key="failed-count" className="failed-count">
-                                <div key="failed-count-text">
-                                    {getIntlText('dashboard.tip.batch_bind_entities_failed', {
-                                        1: failedData?.count || 0,
-                                    })}
-                                </div>
-                                <div
-                                    key="download-wrapper"
-                                    className="download-wrapper"
-                                    onClick={() =>
-                                        handleDownloadErrorFile(commonParams, failedData)
-                                    }
-                                >
-                                    <FileDownloadOutlinedIcon
-                                        key="FileDownloadOutlinedIcon"
-                                        color="inherit"
-                                    />
-                                    <div key="download-text" className="download-text">
-                                        {getIntlText('common.button.download')}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ),
-                confirmButtonText: getIntlText('common.button.confirm'),
-                cancelButtonProps: {
-                    sx: {
-                        display: 'none',
-                    },
-                },
-            });
+                        {
+                            responseType: 'blob',
+                        },
+                    ),
+                );
+                if (error || !isRequestSuccess(resp)) {
+                    return;
+                }
+
+                const data = getResponseData(resp);
+                if (!data) {
+                    return;
+                }
+
+                linkDownload(data, `${dayjs().format('YYYY_MM_DD_HH_mm_ss')}_error_messages.xlsx`);
+                toast.success(getIntlText('common.message.operation_success'));
+            } finally {
+                setDownloading(false);
+            }
         },
+        { wait: 300 },
     );
 
     const handleUploadFile = useMemoizedFn(async (file: FileValueType, callback?: () => void) => {
@@ -180,14 +98,10 @@ export function useBatchBind() {
             return;
         }
 
-        const commonParams = {
-            key: building.key,
-            file: file.original,
-        };
         const [error, resp] = await awaitWrap(
             dashboardAPI.parseToiletBindTemplate({
-                building_key: commonParams.key,
-                file: commonParams.file,
+                building_key: building.key,
+                file: file.original,
             }),
         );
         if (error || !isRequestSuccess(resp)) {
@@ -195,11 +109,18 @@ export function useBatchBind() {
         }
 
         const data = getResponseData(resp);
+        handleSuccessData(data?.success_data);
+
         if (data?.failed_data?.count) {
-            handleFailedData(commonParams, data);
+            setParseResult({
+                key: building.key,
+                file: file.original,
+                data,
+            });
+            setCurrentStatus('completed');
+            return;
         }
 
-        handleSuccessData(data?.success_data);
         callback?.();
     });
 
@@ -208,5 +129,10 @@ export function useBatchBind() {
          * Handle batch bind entities upload file
          */
         handleUploadFile,
+        handleDownloadErrorFile,
+        parseResult,
+        currentStatus,
+        setDownloading,
+        downloading,
     };
 }
