d3f4ea2d3bc0e45aa15a5ea66167db3ec001239f
wenbintian <972619117@qq.com>
2026-01-14 09:57:18 +0800

    fix: The entity export function and loadingButton style

--- apps/web/src/pages/entity/components/export-modal/index.tsx
+++ apps/web/src/pages/entity/components/export-modal/index.tsx
@@ -4,15 +4,17 @@ import { useI18n } from '@milesight/shared/src/hooks';
 import { Modal } from '@milesight/shared/src/components';
 import { DateRangePickerValueType } from '@/components/date-range-picker';
 import { DateRangePicker } from '@/components';
+import { type LoadingButtonProps } from '@milesight/shared/src/components/loading-button';
 
 interface IProps {
     onCancel: () => void;
     onOk: (data: DateRangePickerValueType | null) => void;
+    okButtonProps?: LoadingButtonProps;
 }
 
 const ExportModal = (props: IProps) => {
     const { getIntlText } = useI18n();
-    const { onOk, onCancel } = props;
+    const { onOk, onCancel, okButtonProps } = props;
     const [time, setTime] = useState<DateRangePickerValueType | null>(null);
 
     const handleClose = () => {
@@ -34,6 +36,7 @@ const ExportModal = (props: IProps) => {
             onCancel={handleClose}
             onOk={handleOk}
             title={getIntlText('common.label.export')}
+            okButtonProps={okButtonProps}
         >
             <Box
                 sx={{
